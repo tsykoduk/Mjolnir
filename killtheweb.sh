db406e484e26b542ef24588b352cbfe3b726931a
Greg Nokes <tsykoduk@gmail.com>
2013-04-12 17:45:15 -0700

    adding a loop to control how many times we run

--- killtheweb.sh
+++ killtheweb.sh
@@ -9,12 +9,15 @@ TARGETS=$@
 
 for i in `seq 10`; do # start 20 jobs in parallel
 #	sleep $i && 
-	for t in $TARGETS
+	for times={1..10}
 	do
-		curl -s -S -L -w "%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} speed=%{speed_download} %{url_effective}\\n" "${t}" -o /dev/null
+		for t in $TARGETS
+		do
+			curl -s -S -L -w "%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} speed=%{speed_download} %{url_effective}\\n" "${t}" -o /dev/null
+		done
 	done
-    
 done
+
 
 #Wait for all parallel jobs to finish
 #while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
