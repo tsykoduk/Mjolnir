12c0e39e700a283e3da7646771bd97c3ccee00e2
Greg Nokes <tsykoduk@gmail.com>
2013-04-15 15:43:15 -0700

    ramp it up

--- killtheweb.rb
+++ killtheweb.rb
@@ -3,9 +3,9 @@
 @targets = ARGV
 
 
-10.times do
+100.times do
     fork do
-      10.times do
+      100.times do
         @targets.each do |t|
           system("curl -sSLw \"%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} %{url_effective}\\n\" #{t} -o /dev/null")
           #puts " #{Process.pid} curl -sSLw \"%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} speed=%{speed_download} %{url_effective}\\n\" #{t} -o /dev/null"
