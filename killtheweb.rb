ea43884ab4c5c4a599a18da6000189879f966b11
Greg Nokes <greg@nokes.name>
2013-04-15 21:09:35 -0700

    Update killtheweb.rb

--- killtheweb.rb
+++ killtheweb.rb
@@ -8,7 +8,6 @@
       100.times do
         @targets.each do |t|
           system("curl -sSLw \"%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} %{url_effective}\\n\" #{t} -o /dev/null")
-          #puts " #{Process.pid} curl -sSLw \"%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} speed=%{speed_download} %{url_effective}\\n\" #{t} -o /dev/null"
         end
     end
   end
