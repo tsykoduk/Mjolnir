58147b8eb9be641231b719254f20cb0f95d631ac
Greg Nokes <tsykoduk@gmail.com>
2013-04-19 10:27:40 -0700

    playing with GO for a long term solution

--- killtheweb.go
+++ killtheweb.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 import "strconv"
+import "os/exec"
 
 func main() {
-	for ol := 1; ol <= 10; ol ++ {
-		for il := 1; il <= 10; il ++ {
+	for ol := 1; ol <= 100; ol ++ {
+		for il := 1; il <= 100; il ++ {
 			fmt.Printf("number " + strconv.Itoa(il) + " of run " + strconv.Itoa(ol) + "\n")
 		}
 	}
 }
+
+func tester()
