9b240a445fede4762d28d52531dc6273a53e25af
Greg Nokes <tsykoduk@gmail.com>
2013-04-24 09:06:27 -0700

    Thoughts on rate limiting

--- killtheweb.rb
+++ killtheweb.rb
@@ -3,13 +3,20 @@
 @targets = ARGV
 
 #After some expermentation, 12 forks seems to be optimal. Much more runs up the load on the dyno and 
-# just slows the entire thing down
+# just slows the entire thing down.
+# Needs a little bit more testing, and it should settle between 12 and 14 forks.
+# I need to normalize away diffrences in dyno preformamce by running multi-dyno tests
+# I've seen some very stark diffrences between dynos (~ 500 RPM diffrences in preformance)
 
 12.times do
     fork do
       10000.times do
         @targets.each do |t|
+          # to insure a rate, why not fork here as well, with a sleep after the fork?
+          #fork do
           system("curl -sSLw \"%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} %{url_effective}\\n\" #{t} -o /dev/null")
+          #end
+          #sleep 0.1 should give us 10 requests per second per main fork
       end
     end
   end
