1cb8b5a9782ae7975e0abe127c74565c4e5f7fec
Greg Nokes <tsykoduk@gmail.com>
2014-02-27 11:48:45 -0800

    cleaning up a little, adding a comment or two

--- killtheweb.go
+++ killtheweb.go
@@ -1,5 +1,7 @@
 package main
 
+// Unfinished golang implemtnation of Mjolnir
+
 import "fmt"
 import "strconv"
 import "os/exec"
--- killtheweb.rb
+++ killtheweb.rb
@@ -2,7 +2,7 @@
 
 @targets = ARGV
 
-#After some expermentation, 12 forks seems to be optimal. Much more runs up the load on the dyno and 
+# After some expermentation, 12 forks seems to be optimal. Much more runs up the load on the dyno and
 # just slows the entire thing down.
 # Needs a little bit more testing, and it should settle between 12 and 14 forks.
 # I need to normalize away diffrences in dyno preformamce by running multi-dyno tests
@@ -20,6 +20,7 @@ end
 
 #after everything is done, just sleep the sleep of the victorious.
 #this is just to keep the process from crashing and getting restarted
+
 i = 100
 
 while i < 200 do
